063efae1338330f27a9ce9fcfa5203361693c3db
Pratham Sahu <prathamsahu051@gmail.com>
2024-11-07 16:04:28 +0530

    feat: complete metadata endpoints for user

--- apps/server/src/routes/v1/user.ts
+++ apps/server/src/routes/v1/user.ts
@@ -1,11 +1,62 @@
 import { Request, Response, Router } from "express";
+import { UpdateMetadataSchema } from "../../types";
+import db from "@repo/db/prismaClient"
 
 export const userRouter = Router()
 
-userRouter.get("/metadata", async (req: Request, res: Response) => {
-  
+// This route updates avatar of the user
+userRouter.put("/metadata", userRouter, async (req: Request, res: Response) => {
+  try {
+    const { avatarId } = UpdateMetadataSchema.parse(req.body)
+
+    await db.user.update({
+      where: {
+        id: req.userId
+      },
+      data: {
+        avatarId
+      }
+    })
+
+    res.status(200).json({
+      msg: "Avatar Updation Successfull"
+    })
+    return;
+  } catch (error: any) {
+    res.status(500).json({
+      msg: error.message
+    })
+  }
 })
 
 userRouter.get("/metadata/bulk", async (req: Request, res: Response) => {
-  
+  try {
+    const userIdsString = (req.query.ids ?? "[]") as string
+    const userIds = userIdsString.slice(1, userIdsString.length - 2).split(",")
+
+    const metadata = await db.user.findMany({
+      where: {
+        id: {
+          in: userIds
+        }
+      },
+      select: {
+        avatar: {
+          select: {
+            imageUrl: true
+          }
+        },
+        id: true
+      }
+    })
+
+    res.status(200).json({
+      avatars: metadata.map((user) => ({
+        userId: user.id,
+        imageUrl: user.avatar?.imageUrl
+      }))
+    })
+  } catch (error) {
+    
+  }
 })
