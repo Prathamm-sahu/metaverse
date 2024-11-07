af118c0e9233d83c3a400e80d08c009afbe54a72
Pratham Sahu <prathamsahu051@gmail.com>
2024-11-07 15:03:50 +0530

    feat: created signup endpoint

--- apps/server/src/routes/v1/index.ts
+++ apps/server/src/routes/v1/index.ts
@@ -1,9 +1,51 @@
 import { Router, Request, Response } from "express"
+import { userRouter } from "./user"
+import { spaceRouter } from "./space"
+import { adminRouter } from "./admin"
+import { SignupSchema } from "../../types"
+import { hash } from "../../scrypt"
+import db from '@repo/db/prismaClient'
+import jwt from "jsonwebtoken"
 
 const router = Router()
 
 router.post("/signup", async (req: Request, res: Response) => {
+  try {
+    const parsedBody = SignupSchema.safeParse(req.body)
 
+    if(!parsedBody.success) {
+      res.status(400).json({
+        msg: "Validation Failed"
+      })
+      return;
+    }
+
+    const hashedPassword = await hash(parsedBody.data.password)
+
+    const user = await db.user.create({
+      data: {
+        name: parsedBody.data.name,
+        email: parsedBody.data.email,
+        username: parsedBody.data.username,
+        password: hashedPassword,
+        role: parsedBody.data.type
+      }
+    })
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string)
+
+    res.status(201).json({
+      token,
+      userId: user.id
+    })
+
+    return;
+
+  } catch (error: any) {
+    res.status(500).json({
+      msg: error.msg
+    })
+  }
 })
 router.post("/signin", async (req: Request, res: Response) => {
 
